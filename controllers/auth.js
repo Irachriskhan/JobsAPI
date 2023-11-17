dfb7743cb09f839ccca632d2f168239a0c09ddc9
Christophe Irakoze <irachriskhan@gmail.com>
2023-11-17 15:41:30 +0200

    finalize register page

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,16 +4,21 @@ const { BadRequestError } = require("../errors");
 
 const register = async (req, res) => {
   // res.json(req.body);
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new BadRequestError("Please provide valid name, email and password");
-  }
+  // const { name, email, password } = req.body;
+  // const salt = await bcrypt.genSalt(10); // Random byte
+  // const hashedPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashedPassword };
 
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json(user);
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide valid name, email and password");
+  }
   res.send("You are logged in");
 };
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,8 +22,24 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please, enter the password"],
     minlength: 8,
-    maxlength: 20,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+// process.env.JWT_SECRET;
 
 module.exports = mongoose.model("users", UserSchema);
