24d246407b081d51a75bd479c6a17fb05c85b5b2
Christophe Irakoze <irachriskhan@gmail.com>
2023-11-26 18:39:05 +0200

    working on errors and setting testing tools

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 require("dotenv").config();
 require("express-async-errors");
-const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
 const authenticateUser = require("./middleware/authentication");
+// db connection
+const connectDB = require("./db/connect");
+// routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
-
+// extra security packages
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml")
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+// app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// routers
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,37 @@
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    mag: err.message || "Something went wrong, try again later",
+  };
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  // console.log(Object.values(err.errors));
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ err: err.message });
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field,please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No item foud with id ${err.value}`;
+    customError.statusCode = 404;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
